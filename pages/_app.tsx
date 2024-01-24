3b157c1917297a6a1c67e8f29211b7d8179f2624
vyazemskaya <vyazemskayanna@gmail.com>
2024-01-25 00:58:42 +0300

    frame files

---
+++ pages/_app.tsx
@@ -0,0 +1,23 @@
+import '../styles/globals.css';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import React from 'react';
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>top</title>
+        <link rel='icon' href='/favicon.ico' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
