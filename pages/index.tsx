f89cb510f797f2f6d9d2dc4f8dd1867858ac1c19
vyazemskaya <vyazemskayanna@gmail.com>
2024-01-29 14:05:45 +0300

    add: base components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,30 @@
+import { Button, Htag, P, Tag } from '../components';
+
 export default function Home(): JSX.Element {
-  return <div></div>;
+  return (
+    <>
+      <Htag tag='h1'>text</Htag>
+      <Button appearance='primary' className={'dffg'} arrow='right'>
+        button
+      </Button>
+      <Button appearance='ghost' arrow='down'>
+        button
+      </Button>
+      <P size='m'>
+        Студенты освоят не только hard skills, необходимые для работы
+        веб-дизайнером, но и soft skills — навыки, которые позволят эффективно
+        взаимодействовать в команде с менеджерами, разработчиками и
+        маркетологами. Выпускники факультета могут успешно конкурировать с
+        веб-дизайнерами уровня middle.
+      </P>
+      <Tag size='s'>ghost</Tag>
+      <Tag size='m' color='red'>
+        red
+      </Tag>
+      <Tag size='s' color='green'>
+        green
+      </Tag>
+      <Tag color='primary'>primary</Tag>
+    </>
+  );
 }
