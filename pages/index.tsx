3e79b9100648ca6ee7031f02b417d14bf93c85e7
vyazemskaya <vyazemskayanna@gmail.com>
2024-01-31 20:00:55 +0300

    add: layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 import { Button, Htag, P, Tag, Rating } from '../components';
+import { withLayout } from '../layout/Layout';
 
-export default function Home(): JSX.Element {
+function Home(): JSX.Element {
   const [rating, setRating] = useState<number>(4);
 
   return (
@@ -32,3 +33,5 @@ export default function Home(): JSX.Element {
     </>
   );
 }
+
+export default withLayout(Home);
